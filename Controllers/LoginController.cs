ece1649adf969914568a293a5e17e4371292bb4a
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-12-16 12:10:22 +0300

    Fix some error

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using OWS_Rest_full.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace OWS_Rest_full.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController: ControllerBase 
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin) 
+        {
+            var user = Authenticate(userLogin);
+            if (user != null)
+            {
+                var token = Generate(user);
+                var collection = new Dictionary<string, object>();
+                collection.Add("succes", true);
+                collection.Add("token", token);
+                return Ok(collection);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentails = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.Surname),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentails
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(o => o.Username.ToLower() == userLogin.Username.ToLower() && o.Password == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+    }
+}
